ae5147638604b75504a99532e977f6cba529cc02
Max Kuzmenko <kuzmenko.maks.1999@gmail.com>
2024-10-04 16:37:18 +0300

    feat: v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
 import AddTodo from './AddTodo';
 import TodoItem from './TodoItem';
-import RadioButtonGroup from './RadioButtonGroup';
-import { getTodos } from "./api";
+import { getTodos, updateUserTodos } from "./api";
 import { trackFirstClicks } from "./tracking";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [type, setType] = useState('fetched');
 
-  let userClicks = 0;
+  const { userId, topicId } = useParams();
 
-  const filter = { type };
+  const todosApiParams = { topic: topicId, user: userId };
 
   useEffect(() => {
-    const request = getTodos(filter);
+    const request = getTodos(todosApiParams);
     request.then(response => response.json());
-    request.then(data => setTodos(data.map(todo => ({ ...todo, type: 'fetched' }))));
-  }, [filter]);
+    request.then(userTodos => setTodos(userTodos));
+  }, [todosApiParams]);
+
+  let userClicks = 0;
 
   useEffect(() => {
     setTimeout(() => {
@@ -30,27 +32,27 @@ function App() {
     const newTodo = { id: todos.length + 1, name: name, type: 'my' };
     todos.unshift(newTodo);
     setTodos(todos);
+
+    updateUserTodos(userId, topicId, todos);
   };
 
   const handleRemoveTodo = (id) => {
     userClicks += 1;
     const updatedTodos = todos.filter(todo => todo.id !== id);
     setTodos(updatedTodos);
+
+    updateUserTodos(userId, topicId, updatedTodos);
   };
 
   return (
     <main className="page">
       <h1>Todo App</h1>
-      <RadioButtonGroup
-        options={['fetched', 'my']}
-        onSelect={(opt) => setType(opt)}
-      />
       <AddTodo onAdd={handleAddTodo} />
       <ul className="todo-list">
         {todos.map(todo => (
-          <li className="todo-item">
+          <div className="todo-item">
             <TodoItem todo={todo} onRemove={handleRemoveTodo} />
-          </li>
+          </div>
         ))}
       </ul>
     </main>
--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
 
-export const getTodos = (filter) => {
+export const getTodos = (params) => {
   return fetch(`https://65b8c458b71048505a896622.mockapi.io/api/v1/todos`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Test-filter': JSON.stringify(filter)
+      'Test-filter': JSON.stringify(params)
     }
+  })
+}
+
+export const updateUserTodos = (userId, topicId, todos) => {
+  return fetch(`https://65b8c458b71048505a896622.mockapi.io/api/v1/todos`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ userId, topicId, todos })
   })
 }
